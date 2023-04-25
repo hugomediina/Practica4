3b4d3f9684cbea6a4ad9e50604a60494eff073fc
hugomediina <114917231+hugomediina@users.noreply.github.com>
2023-04-25 14:04:39 +0200

    Primeros pasos en javafx

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,22 +1,24 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import uji.al415648.datos.Row;
+import uji.al415648.datos.Table;
 
 public class Hello extends Application {
-
+    private Table table;
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        StackPane root = new StackPane();
-        Button btn = new Button("Hola");
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 250, 250));
-        primaryStage.show();
+        primaryStage.setTitle("Inicio de sesión");
+        ObservableList<Row> list= FXCollections.observableArrayList(table.getRowAt(8),table.getRowAt(2),table.getRowAt(1));
+        ComboBox lista=new ComboBox<>(list);
     }
 }
